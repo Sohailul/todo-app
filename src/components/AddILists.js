265eec5b47cad47d42c051d8a6ac5a97258b3a91
sohailul <sohailulalam@gmail.com>
2022-05-19 02:40:53 +0600

    To do app designed and apis implemented

---
+++ src/components/AddILists.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const AddILists = () => {
+
+    const handleAddList = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const description = event.target.description.value;
+
+        const list = { name, description };
+
+        fetch('http://localhost:5000/list', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(list)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                alert("List added Successfully");
+                event.target.reset();
+            })
+
+    }
+    return (
+        <div className='mb-5'>
+            <div className="card m-auto mt-5" style={{ width: "25rem" }}>
+                <div className="card-header"><h1 className='text-info text-center'>REACT TODO APP</h1></div>
+                <div className="card-body">
+                    <form onSubmit={handleAddList}>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input type="text" className="form-control" name='name' placeholder="Enter name" required />
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Description</label>
+                            <textarea className="form-control" name='description' placeholder="Description"></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-info w-100 mt-3 fw-bold">Add task</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AddILists;
---
+++ src/components/ManageLists.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+const ManageLists = () => {
+    const [lists, setLists] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/list')
+            .then(res => res.json())
+            .then(data => setLists(data))
+    }, [])
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure?');
+
+        if (proceed) {
+            const url = `http://localhost:5000/list/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = lists.filter(list => list._id !== id)
+                    setLists(remaining);
+                })
+        }
+    }
+    return (
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">SL</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        lists.map(list =>
+                            <>
+                                <tr key={list._id}>
+                                    <th scope="row">1</th>
+                                    <td>{list.name}</td>
+                                    <td>{list.description}</td>
+                                    <td><button onClick={() => handleDelete(list._id)} className='btn btn-danger'>Delete</button></td>
+                                </tr>
+                            </>)
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ManageLists;
